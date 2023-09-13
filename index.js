4f05f4e3339ba871f709089b880799da0e0862db
redpalico <red.palico@outlook.com>
2023-09-14 04:57:25 +0900

    2nd commit

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Initialize the database
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+  db.run("CREATE TABLE user_data (info TEXT, timestamp TEXT)");
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.post('/', (req, res) => {
   const userInput = req.body.userInput;
-  res.send(userInput + 'AAA');
+  const timestamp = new Date().toISOString();
+
+  db.run("INSERT INTO user_data VALUES (?, ?)", [userInput, timestamp], function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    console.log(`A new row has been inserted with rowid ${this.lastID}`);
+  });
+
+  res.send(`${userInput}AAA - ${timestamp}`);
 });
 
 app.listen(port, () => {
